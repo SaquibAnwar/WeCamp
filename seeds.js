9b24f7b0e90c88c6d24f018036feb8b3e55cf48e
SaquibAnwar <saquibedu@gmail.com>
2019-04-19 13:12:41 +0530

    Adding Dependencies

--- seeds.js
+++ seeds.js
@@ -1,3 +1,4 @@
+// USED TO INITIALIZE CAMPGROUNGD
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
@@ -7,7 +8,7 @@ var data = [
             name: "Cloud's Rest",
             image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
             description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        },       
+        },
         {
             name: "Desert Mesa",
             image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
@@ -26,8 +27,8 @@ function seedDB(){
     Campground.remove({}, function(err){
         if(err)
             console.log(err);
-        console.log("removed campgrounds!"); 
-        
+        console.log("removed campgrounds!");
+
         //Add a few capmgrounds
         data.forEach(function(seed){
             Campground.create(seed, function(err, campground){
@@ -35,7 +36,7 @@ function seedDB(){
                     console.log(err);
                 else{
                     console.log("added a campground");
-                    
+
                     // Create a comment
                     Comment.create(
                         {
@@ -53,12 +54,12 @@ function seedDB(){
                 }
             });
         });
-        
+
     });
-    
-    
+
+
     // Add a few comments
-    
+
 }
 
-module.exports = seedDB; 
+module.exports = seedDB;
